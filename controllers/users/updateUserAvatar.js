aac1eb5a53747733a1fe640cecab779274c8b398
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-12-10 17:32:48 +0200

    added file validation

--- controllers/users/updateUserAvatar.js
+++ controllers/users/updateUserAvatar.js
@@ -1,18 +1,28 @@
 const fs = require('fs').promises
 const path = require('path')
+const createError = require('http-errors')
 const { User } = require('../../model')
 const imageOptimization = require('../../utils/imageOptimization')
+
+const VALID_FILE_TYPES = ['png', 'jpg', 'jpeg', 'tiff', 'WebP', 'gif']
 
 const storageDir = path.join(process.cwd(), 'public', 'avatars')
 
 const updateUserAvatar = async (req, res) => {
   const { id } = req.user
+
+  if (!req.file) throw createError(400, 'File missing')
+
   const { path: temporaryName, originalname } = req.file
 
-  const newUniqueName = `${id}-${originalname}`
-  const destinationName = path.join(storageDir, newUniqueName)
-
   try {
+    const [fileExtension] = originalname.split('.').reverse()
+
+    if (!VALID_FILE_TYPES.includes(fileExtension)) throw createError(400, 'Invalid file type')
+
+    const newUniqueName = `${id}-${originalname}`
+    const destinationName = path.join(storageDir, newUniqueName)
+
     fs.rename(temporaryName, destinationName)
     imageOptimization(destinationName)
 
