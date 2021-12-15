bdddbf48faacfd32f0bbe681e8c4135c396392b1
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-12-15 22:25:24 +0200

    update

--- utils/createUrl.js
+++ utils/createUrl.js
@@ -8,7 +8,7 @@ const createUrl = (req, path, filenameOrData) => {
 
   const urlPrepack = Object.values(urlParams).join('/')
 
-  return new URL(urlPrepack)
+  return new URL(urlPrepack.toString())
 }
 
 module.exports = createUrl
